0b6cc9d7abc3b866b41ecfb00b596a7a46173042
Eric Hartmann <hartmann.eric@gmail.com>
2017-12-18 15:50:18 +0100

    Remove abstract class DirectoryObject

--- src/main/java/org/almrangers/auth/aad/AadGroup.java
+++ src/main/java/org/almrangers/auth/aad/AadGroup.java
@@ -26,37 +26,31 @@
  */
 package org.almrangers.auth.aad;
 
-public class AadGroup extends DirectoryObject {
-  protected String objectId;
-  protected String objectType;
-  protected String displayName;
+public class AadGroup {
+  private String objectId;
+  private String objectType;
+  private String displayName;
 
-  @Override
   public String getObjectId() {
     return objectId;
   }
 
-  @Override
   public void setObjectId(String objectId) {
     this.objectId = objectId;
   }
 
-  @Override
   public String getObjectType() {
     return objectType;
   }
 
-  @Override
   public void setObjectType(String objectType) {
     this.objectType = objectType;
   }
 
-  @Override
   public String getDisplayName() {
     return displayName;
   }
 
-  @Override
   public void setDisplayName(String displayName) {
     this.displayName = displayName;
   }
